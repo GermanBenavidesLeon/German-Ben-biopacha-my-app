3fccd94003ef5a961aee478815caa4dafdbc8b67
GermanBenavidesLeon <manchitobena@gmail.com>
2021-10-30 18:25:01 -0300

    se creo ItemCount y agrego una nueva libreria

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import NavBar from './components/NavBar/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <NavBar   />
       <ItemListContainer greeting='Bienvenidos a Bio Pacha E-commerce' />
+      <ItemCount initial={1} stock={5} />
     </div>
   );
 }
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Button } from 'react-bootstrap'; 
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import './ItemCount.css';
+import {useState} from 'react';
+
+// Importe una libreria para incluir nuevos alert y no los de siempre
+
+function ItemCount({stock, initial}) {
+    const [count, setCount] = useState(initial)
+
+    const sumarItem = () =>{
+        count < stock ? setCount(count + 1) : Swal.fire(   // este es un alert de la libreria q instale
+            'Ooppss!',
+            'Limite de productos!',
+            'warning'
+          )
+    }
+
+    const restarItem = () =>{
+        count > initial ? setCount(count - 1) : Swal.fire(      // este es un alert de la libreria q instale
+            'Por favor!',
+            'Debes agregar al menos un producto!',
+            'error'
+          )
+    }
+
+    const onAdd = () => {
+        const MySwal = withReactContent(Swal)   // este es un alert de la libreria q instale
+        MySwal.fire({
+            didOpen: () => {
+        MySwal.clickConfirm()
+      }
+        }).then(() => {
+            return MySwal.fire(<p>agregaste {count} productos al carrito</p>)
+        })
+}
+
+    return (
+        <div className="classCount">
+            <Button variant="outline-success" onClick={sumarItem}>+</Button>
+            <label>{count}</label>
+            <Button variant="outline-danger" onClick={restarItem}>-</Button>
+            <Button variant="outline-primary" onClick={onAdd}>Agregar al Carrito</Button>
+        </div>
+    )
+}
+
+export default ItemCount
